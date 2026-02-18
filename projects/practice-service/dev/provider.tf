b7e6bebbe3d5cec5059f267828439da9c3cc7653
sammool <sammool2003@gmail.com>
2026-02-18 21:48:59 +0900

    feat: use module

--- projects/practice-service/dev/provider.tf
+++ projects/practice-service/dev/provider.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-  region = "ap-northeast-2" # 서울 리전
+  region  = "ap-northeast-2" # 서울 리전
   profile = "default"
 }
 
-terraform{
-    required_version = "~>1.5"
+terraform {
+  required_version = "~>1.5"
 
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 5.9"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.9"
     }
+  }
 }
